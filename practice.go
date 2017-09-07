d867e20e2aebebc1b5ec6cdebce6bb591b93f760
Matthew Downs <mwdowns@gmail.com>
2017-09-07 15:14:30 -0400

    struct practice

--- practice.go
+++ practice.go
@@ -6,5 +6,13 @@ var something string = "hello"
 const something2 = "world"
 
 func main()  {
-    fmt.Println(something, something2)
+    
+    message1 := message{greeting: "hello", person: "cleveland"}
+    
+    fmt.Println(message1.greeting, message1.person + "!")
+}
+
+type message struct {
+    greeting string
+    person string
 }
