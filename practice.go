03eaa3f68cf81a09cbaa4e044fa0d0c88de1b2d4
Matthew Downs <mwdowns@gmail.com>
2017-09-07 19:15:25 -0400

    trying some stuff with structs and functions

--- practice.go
+++ practice.go
@@ -7,26 +7,44 @@ const something2 = "world"
 
 func main()  {
     
-    data := message{"hello", "cleveland"}
-    message1 := printMessage(data.greeting, data.name)
-    fmt.Println(message1)
+    // data := cityInfo{"hello", "cleveland"}
+    // message1 := printMessage(data.greeting, data.name)
+    // fmt.Println(message1)
     
-    winPer,city := newFunction(2, 16, data)
-    fmt.Printf("the %v football team had a %v winning percentage last year.\n", city, winPer)
+    // winPer,city := newFunction(2, 16, data)
+    // fmt.Printf("the %v football team had a %v winning percentage last year.\n", city, winPer)
+    
+    city1 := cityInfo{"hello", "cleveland", 2, 16}
+    city2 := cityInfo{"good morning", "atlanta", 15, 19}
+    city3 := cityInfo{"fuck off", "boston", 16, 19}
+    
+    helloCity(city1.name, city2.name, city3.name)
+    fmt.Println("\n")
+    cities :=[]string { city1.name, city2.name, city3.name }
+    helloCity(cities...)
     
 }
 
-type message struct {
+type cityInfo struct {
     greeting string
     name string
+    wins float32
+    gamesPlayed float32
 }
 
 func printMessage(var1 string, var2 string) string {
     return var1 + " " + var2 + "!"
 }
 
-func newFunction(wins float32, games float32, city message) (winPer float32, cityName string) {
+func newFunction(city cityInfo) (winPer float32, cityName string) {
     cityName = city.name
-    winPer = wins / games
+    winPer = city.wins / city.gamesPlayed
     return
+}
+
+func helloCity(cities ...string) {
+    for _,city := range cities {
+        message := printMessage("hello", city)
+        fmt.Println(message)
+    }
 }
