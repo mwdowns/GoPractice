7cc03b3a1fe863d6f6007def0f20998839303fcd
mwdowns <mwdowns@gmail.com>
2018-04-15 15:18:06 -0400

    adds deck.go file for manipulating a deck type

---
+++ cards/deck.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+// create a new type of deck
+// which is a slice of strings
+
+type deck []string
+
+// type card struct {
+// 	name string
+// 	value int
+// }
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Hearts", "Spades", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "King", "Queen", "Jack", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
+func (d deck) cardPrint() {
+	for _, card := range d {
+		fmt.Println(card)
+	}
+}
