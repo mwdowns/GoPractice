d672a08e63d4a76d2f3a5c9bbd5924d9e1d5a879
mwdowns <mwdowns@gmail.com>
2018-04-17 15:30:32 -0400

    adds shuffle and randomizer

--- cards/deck.go
+++ cards/deck.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // create a new type of deck
@@ -60,9 +61,11 @@ func newDeckFromFile(fileName string) deck {
 }
 
 func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
 	for i := range d {
 		//generate a random number from -1 to len of deck
-		newPosition := rand.Intn(len(d) - 1)
+		newPosition := r.Intn(len(d) - 1)
 		d[i], d[newPosition] = d[newPosition], d[i]
 	}
 }
