27aed2bb90b8733d38f9ec4acdcfd401d6902564
mwdowns <mwdowns@gmail.com>
2018-04-19 19:45:48 -0400

    adds two new tests and some logging of passed tests

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -10,6 +10,15 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 52 {
 		t.Errorf("Expected deck length of 52, but got %v", len(d))
 	}
+	fmt.Println("len test passed")
+	if d[0] != "Ace of Hearts" {
+		t.Errorf("Expected Ace of Hearts as first card, got %v", d[0])
+	}
+	fmt.Println("First card test passed")
+	if d[len(d)-1] != "Ten of Clubs" {
+		t.Errorf("Expected Ten of Clubs as last card, got %v", d[len(d)-1])
+	}
+	fmt.Println("Last card test passed")
 }
 
 func TestShuffle(t *testing.T) {
@@ -19,13 +28,13 @@ func TestShuffle(t *testing.T) {
 	count := 0
 	for i := range d {
 		if d[i] == d2[i] {
-			count += 1
+			count++
 		}
 	}
-	fmt.Println(count)
 	if count > 25 {
 		t.Errorf("Not very random. %v matches", count)
 	}
+	fmt.Println("Ranomizer test passed")
 }
 
 func TestDeal(t *testing.T) {
@@ -35,7 +44,9 @@ func TestDeal(t *testing.T) {
 	if len(hand) != num {
 		t.Errorf("Expected hand of 5 cards, got %v", len(hand))
 	}
+	fmt.Println("Hand len test passed")
 	if len(remainingDeck) != len(d)-num {
 		t.Errorf("Expected remainingDeck to be 47, got %v", len(remainingDeck))
 	}
+	fmt.Println("Remaining deck len test passed")
 }
