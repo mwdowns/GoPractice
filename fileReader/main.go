9fe240a2b583335bca4ccb75788a57e547c332da
mwdowns <mwdowns@gmail.com>
2018-05-09 15:07:18 -0400

    adds code for fileReader assignment

--- fileReader/main.go
+++ fileReader/main.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
+type fileReader struct{}
+
 func main() {
-	fmt.Println("yo, read this!")
-	fmt.Println(os.Args[1])
+
+	resp, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	fr := fileReader{}
+	io.Copy(fr, resp)
+}
+
+func (fileReader) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("the byte slice was this long: ", len(bs))
+	return len(bs), nil
 }
