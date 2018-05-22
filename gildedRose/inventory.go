3a71d37ba1939243b4eebcf9100419abd9e235fd
mwdowns <mwdowns@gmail.com>
2018-05-22 14:25:03 -0400

    adds random inventory creator function

--- gildedRose/inventory.go
+++ gildedRose/inventory.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 type item struct {
@@ -26,5 +28,58 @@ func newInventory() inventory {
 }
 
 func (i inventory) printInventory() {
-	fmt.Println(i)
+	if len(i.EpicItems) == 0 {
+		fmt.Println("There are no Epic Items. Try back later.")
+	} else {
+		fmt.Printf("Epic Items: %v\n", i.EpicItems)
+	}
+	if len(i.SpecialItems) == 0 {
+		fmt.Println("There are no Special Items. Try back later.")
+	} else {
+		fmt.Printf("Special Items: %v\n", i.SpecialItems)
+	}
+	if len(i.ConjuredItems) == 0 {
+		fmt.Println("There are no Conjured Items. Try back later.")
+	} else {
+		fmt.Printf("Conjured Items: %v\n", i.ConjuredItems)
+	}
+	if len(i.NormalItems) == 0 {
+		fmt.Println("There are no Normal Items. Try back later.")
+	} else {
+		fmt.Printf("Normal Items: %v\n", i.NormalItems)
+	}
+}
+
+func (i item) printItem() {
+	fmt.Printf("Name: %v\n", i.Name)
+	fmt.Printf("Sell in %v days.\n", i.SellBy)
+	fmt.Printf("Quality: %v\n", i.Quality)
+}
+
+func inventoryRandomizer(seed int) inventory {
+	randomInventory := inventory{}
+	Objects := []string{"Sword", "Potion", "Salve", "Sheild", "Gauntlets", "Boots"}
+	Adjectives := []string{"Awesome", "Pretty Good", "Excellent", "Bodacious", "OK", "Sub-par"}
+	Attributes := []string{"Speed", "Power", "Luck", "Healing", "Invisibility", "Stone", "Fire"}
+	for i := 0; i < seed; i++ {
+		source := rand.NewSource(time.Now().UnixNano())
+		r := rand.New(source)
+		object := Objects[r.Intn(len(Objects)-1)]
+		adj := Adjectives[r.Intn(len(Adjectives)-1)]
+		attr := Attributes[r.Intn(len(Attributes)-1)]
+		itemName := object + " of " + adj + " " + attr
+		itemType := r.Intn(100)
+		if itemType == 1 {
+			randomInventory.EpicItems = append(randomInventory.EpicItems, item{Name: itemName, SellBy: 10, Quality: 80})
+		}
+		if itemType < 5 {
+			randomInventory.SpecialItems = append(randomInventory.SpecialItems, item{Name: itemName, SellBy: 10, Quality: 5})
+		}
+		if itemType < 20 {
+			randomInventory.ConjuredItems = append(randomInventory.ConjuredItems, item{Name: itemName, SellBy: 10, Quality: 20})
+		} else {
+			randomInventory.NormalItems = append(randomInventory.NormalItems, item{Name: itemName, SellBy: 10, Quality: 10})
+		}
+	}
+	return randomInventory
 }
--- gildedRose/main.go
+++ gildedRose/main.go
@@ -6,10 +6,12 @@ func main() {
 
 	swordOfPrettyGoodStats := newItem("Sword of Pretty Good Stats", 10, 10)
 
-	inventory := newInventory()
-
+	inventory := inventoryRandomizer(10)
+	inventory2 := newInventory()
 	inventory.NormalItems = append(inventory.NormalItems, swordOfPrettyGoodStats)
 
 	fmt.Println("Doin' the guilded rose thing in Go!")
+	inventory2.printInventory()
+	swordOfPrettyGoodStats.printItem()
 	inventory.printInventory()
 }
