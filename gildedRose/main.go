362d675c96e5018ee4d2b0adac4c264dc70c8c17
mwdowns <mwdowns@gmail.com>
2018-05-23 15:56:04 -0400

    adds print player function

--- gildedRose/main.go
+++ gildedRose/main.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 )
 
+var answer string
+
 func main() {
 
 	fmt.Println("Doin' the guilded rose thing in Go!")
 	fmt.Println("Want to play the game? (y/n) ")
-	var answer string
 	fmt.Scanf("%s", &answer)
 	if answer == "y" {
 		fmt.Println("Great")
@@ -24,4 +25,16 @@ func main() {
 func startGame() {
 	inventory := inventoryRandomizer(15)
 	inventory.printInventory()
+	fmt.Println("Would you like to create a player? (y/n)")
+	fmt.Scanf("%s", &answer)
+	if answer == "y" {
+		fmt.Println("Great")
+		p := createNewPlayer()
+		p.printPlayer()
+	} else if answer == "n" {
+		fmt.Println("OK. See you later")
+	} else {
+		fmt.Println("Please choose y or n")
+		startGame()
+	}
 }
--- gildedRose/player.go
+++ gildedRose/player.go
@@ -25,7 +25,7 @@ func createNewPlayer() player {
 	var class string
 	fmt.Println("What name would you like to give your player? ")
 	fmt.Scanln("%s", &name)
-	fmt.Println("What class would you like to give your player? (for a list of classes, type 'help'")
+	fmt.Println("What class would you like to give your player? (for a list of classes, type 'help')")
 	fmt.Scanln("%s", &class)
 	if class == "help" {
 		fmt.Println("Scoundrel - Engineer - Captain")
@@ -50,4 +50,16 @@ func createNewPlayer() player {
 	p.Weapon = p.Inventory.NormalItems[1]
 	p.Armor = p.Inventory.NormalItems[2]
 	return p
+}
+
+func (p player) printPlayer() {
+	fmt.Printf("Your player's name is %v", p.Name)
+	fmt.Printf("They are a %v class.", p.Class)
+	fmt.Printf("Their maing weapon is a %v", p.Weapon.Name)
+	fmt.Printf("They are wearing a %v", p.Armor.Name)
+	fmt.Println("Their inventory consists of these items:")
+	for _, i := range p.Inventory.NormalItems {
+		fmt.Println(i.Name)
+	}
+	fmt.Printf("They have on special item %v", p.Inventory.IonizedItems[0].Name)
 }
